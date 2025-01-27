a3492400f7206d4fece9daab26a6b832c6aa8ff2
Devopslearn2023 <123951775+Devopslearn2023@users.noreply.github.com>
2025-01-27 17:05:35 +0530

    update #1

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,8 +1,8 @@
 import os
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
-    dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
-    dbhost=os.getenv('AZURE_POSTGRESQL_HOST'),
-    dbname=os.getenv('AZURE_POSTGRESQL_NAME')
+    dbuser='hytxpkpscy',
+    dbpass='VA1a$Ptnkw6foIJ$',
+    dbhost='testdbserver2701.postgres.database.azure.com',
+    dbname='test-database'
 )
